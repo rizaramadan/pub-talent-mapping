ba6cba002abf81f96f9bb0cf5c4c2c3219b51ba3
Riza Ramadan <riza.ramadan@gmail.com>
2024-11-10 09:40:23 +0700

    feat: done build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,12 @@
 "use client";
 
-import { useEffect } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import Home from "./Home";
 
-//http://localhost:4000/?user-id=donwafiqo@gmail.com&fullname=abc
-
-export default function Home() {
-  
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  
-  useEffect(() => {
-    const fetchAndRedirect = async () => {
-      // Get URL parameters
-      const userId = searchParams.get('user-id');
-      const fullname = searchParams.get('fullname');
-
-      // Validate parameters
-      if (!userId || !fullname) {
-        router.push("/input");
-        return;
-      }
-
-      try {
-        const url = `/api/get-result/${userId}`;
-        console.log(url);
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log(data);
-
-        if (!data || Object.keys(data).length === 0 || !data.userId) {
-          // No result exists, redirect to quiz page with parameters
-          router.push(`/quiz?user-id=${userId}&fullname=${fullname}`);
-        } else {
-          // Result exists, redirect to result page
-          localStorage.setItem('resultData', JSON.stringify(data));
-          router.push(`/result`);
-        }
-      } catch (error) {
-        console.error('Error fetching result:', error);
-        //router.push("/sorry");
-      }
-    };
-
-    fetchAndRedirect();
-  }, [router, searchParams]);
-
-  return null;
+export default function RootHome() {
+  return (
+    <Suspense>
+      <Home />
+    </Suspense>
+  );
 }
