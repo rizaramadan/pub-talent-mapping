6833aacf5f7f139865353edb9f23ef21bd99d097
Hermanet Lay <laychinhan@gmail.com>
2024-11-11 16:53:44 +0700

    chore: disable hydration warning on body
    chore: make answer check more efficient
    
    fix: scrolling issues caused by disabled button

--- src/app/quiz/old-page.tsx
+++ src/app/quiz/old-page.tsx
@@ -70,6 +70,10 @@ export default function Quiz() {
             .then(data => setQuiz(data));
     }, []);
 
+  useEffect(() => {
+    console.log('called')
+  }, []);
+
     const handleAnswerChange = (questionNumber: number, answer: string) => {
         setAnswers(prev => ({
             ...prev,
