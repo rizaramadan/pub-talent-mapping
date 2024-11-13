aceea3aa346b7c1ec63dbac1e5247fe57da15a6e
Riza Ramadan <riza.ramadan@gmail.com>
2024-11-13 08:08:22 +0700

    feat: add sentry monitoring

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://2a9a31ef291fd17b9fa5dfaed6ab2db8@o4506859215519744.ingest.us.sentry.io/4508288213450752",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
